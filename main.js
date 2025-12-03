2271f8fe81b3a41b1c8ff065c575cc1f1b0ab268
Coder Somagam <sahebx372@gmail.com>
2025-12-04 04:18:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-//base by Tech-God
+//base by saheb bot program
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @techgod143
-//Instagram: techgod143
-//Telegram: t.me/techgod143
-//GitHub: @techgod143
-//WhatsApp: +917466008456
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@techgod143
+//YouTube: Web man
+//WhatsApp: +917718570357 
+//want more free bot scripts? 
 
 require('./settings')
 const pino = require('pino')
@@ -18,7 +15,7 @@ const axios = require('axios')
 const PhoneNumber = require('awesome-phonenumber')
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif')
 const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetch, await, sleep, reSize } = require('./lib/myfunc')
-const { default: XeonBotIncConnect, delay, PHONENUMBER_MCC, makeCacheableSignalKeyStore, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@whiskeysockets/baileys")
+const { default: SAHEBbotIncConnect, delay, PHONENUMBER_MCC, makeCacheableSignalKeyStore, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@whiskeysockets/baileys")
 const NodeCache = require("node-cache")
 const Pino = require("pino")
 const readline = require("readline")
@@ -32,7 +29,7 @@ const store = makeInMemoryStore({
     })
 })
 
-let phoneNumber = "911234567890"
+let phoneNumber = "917718570357"
 let owner = JSON.parse(fs.readFileSync('./database/owner.json'))
 
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
@@ -41,12 +38,12 @@ const useMobile = process.argv.includes("--mobile")
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
 const question = (text) => new Promise((resolve) => rl.question(text, resolve))
          
-async function startXeonBotInc() {
+async function startSahebBotInc() {
 //------------------------------------------------------
 let { version, isLatest } = await fetchLatestBaileysVersion()
 const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
     const msgRetryCounterCache = new NodeCache() // for retry message, "waiting message"
-    const XeonBotInc = makeWASocket({
+    const SahebBotInc = makeWASocket({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: !pairingCode, // popping up QR in terminal log
       browser: [ "Ubuntu", "Chrome", "20.0.04" ], // for this issues https://github.com/WhiskeySockets/Baileys/issues/328
@@ -102,7 +99,7 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
       }, 3000)
    }
 
-    XeonBotInc.ev.on('messages.upsert', async chatUpdate => {
+    SahebBotInc.ev.on('messages.upsert', async chatUpdate => {
         //console.log(JSON.stringify(chatUpdate, undefined, 2))
         try {
             const mek = chatUpdate.messages[0]
@@ -119,7 +116,7 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
     })
     
     //autostatus view
-        XeonBotInc.ev.on('messages.upsert', async chatUpdate => {
+        SahebBotInc.ev.on('messages.upsert', async chatUpdate => {
         	if (global.autoswview){
             mek = chatUpdate.messages[0]
             if (mek.key && mek.key.remoteJid === 'status@broadcast') {
@@ -128,7 +125,7 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
     })
 
    
-    XeonBotInc.decodeJid = (jid) => {
+    SahebBotInc.decodeJid = (jid) => {
         if (!jid) return jid
         if (/:\d+@/gi.test(jid)) {
             let decode = jidDecode(jid) || {}
@@ -136,7 +133,7 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
         } else return jid
     }
 
-    XeonBotInc.ev.on('contacts.update', update => {
+    SahebBotInc.ev.on('contacts.update', update => {
         for (let contact of update) {
             let id = XeonBotInc.decodeJid(contact.id)
             if (store && store.contacts) store.contacts[id] = {
@@ -146,9 +143,9 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
         }
     })
 
-    XeonBotInc.getName = (jid, withoutContact = false) => {
-        id = XeonBotInc.decodeJid(jid)
-        withoutContact = XeonBotInc.withoutContact || withoutContact
+    SahebBotInc.getName = (jid, withoutContact = false) => {
+        id = SahebBotInc.decodeJid(jid)
+        withoutContact = SahebBotInc.withoutContact || withoutContact
         let v
         if (id.endsWith("@g.us")) return new Promise(async (resolve) => {
             v = store.contacts[id] || {}
@@ -158,15 +155,15 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
         else v = id === '0@s.whatsapp.net' ? {
                 id,
                 name: 'WhatsApp'
-            } : id === XeonBotInc.decodeJid(XeonBotInc.user.id) ?
-            XeonBotInc.user :
+            } : id === SahebBotInc.decodeJid(XeonBotInc.user.id) ?
+            SahebBotInc.user :
             (store.contacts[id] || {})
         return (withoutContact ? '' : v.name) || v.subject || v.verifiedName || PhoneNumber('+' + jid.replace('@s.whatsapp.net', '')).getNumber('international')
     }
     
-    XeonBotInc.public = true
+    SahebBotInc.public = true
 
-    XeonBotInc.serializeM = (m) => smsg(XeonBotInc, m, store)
+    SahebBotInc.serializeM = (m) => smsg(SahebBotInc, m, store)
 
 XeonBotInc.ev.on("connection.update",async  (s) => {
         const { connection, lastDisconnect } = s
@@ -191,24 +188,24 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
             startXeonBotInc()
         }
     })
-    XeonBotInc.ev.on('creds.update', saveCreds)
-    XeonBotInc.ev.on("messages.upsert",  () => { })
+    SahebBotInc.ev.on('creds.update', saveCreds)
+    SahebBotInc.ev.on("messages.upsert",  () => { })
 
-    XeonBotInc.sendText = (jid, text, quoted = '', options) => XeonBotInc.sendMessage(jid, {
+    SahebBotInc.sendText = (jid, text, quoted = '', options) => XeonBotInc.sendMessage(jid, {
         text: text,
         ...options
     }, {
         quoted,
         ...options
     })
-    XeonBotInc.sendTextWithMentions = async (jid, text, quoted, options = {}) => XeonBotInc.sendMessage(jid, {
+    SahebBotInc.sendTextWithMentions = async (jid, text, quoted, options = {}) => XeonBotInc.sendMessage(jid, {
         text: text,
         mentions: [...text.matchAll(/@(\d{0,16})/g)].map(v => v[1] + '@s.whatsapp.net'),
         ...options
     }, {
         quoted
     })
-    XeonBotInc.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
+    SahebBotInc.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
         let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,` [1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
         let buffer
         if (options && (options.packname || options.author)) {
@@ -217,7 +214,7 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
             buffer = await imageToWebp(buff)
         }
 
-        await XeonBotInc.sendMessage(jid, {
+        await SahebBotInc.sendMessage(jid, {
             sticker: {
                 url: buffer
             },
@@ -227,7 +224,7 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
         })
         return buffer
     }
-    XeonBotInc.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
+    SahebBotInc.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
         let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,` [1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
         let buffer
         if (options && (options.packname || options.author)) {
@@ -236,7 +233,7 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
             buffer = await videoToWebp(buff)
         }
 
-        await XeonBotInc.sendMessage(jid, {
+        await SahebBotInc.sendMessage(jid, {
             sticker: {
                 url: buffer
             },
@@ -246,7 +243,7 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
         })
         return buffer
     }
-    XeonBotInc.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
+    SahebBotInc.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
         let quoted = message.msg ? message.msg : message
         let mime = (message.msg || message).mimetype || ''
         let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
@@ -262,7 +259,7 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
         return trueFileName
     }
 
-    XeonBotInc.downloadMediaMessage = async (message) => {
+    SahebBotInc.downloadMediaMessage = async (message) => {
         let mime = (message.msg || message).mimetype || ''
         let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
         const stream = await downloadContentFromMessage(message, messageType)
@@ -295,4 +292,5 @@ if (e.includes("Connection Closed")) return
 if (e.includes("Timed Out")) return
 if (e.includes("Value not found")) return
 console.log('Caught exception: ', err)
+
 })
